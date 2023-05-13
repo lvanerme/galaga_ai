2c2923fc3f847de137dea56f53caee5f7fb70cab
lvanerme <103009757+lvanerme@users.noreply.github.com>
2023-05-13 12:03:17 -0400

    changes for server testing

--- ga.py
+++ ga.py
@@ -164,7 +164,7 @@ def ga(pop_size, cross_rate=0.7, mut_rate=0.03, max_iters=20, net_units=8, net_u
     start = time.time()
     players = gen_seed(net_units, net_units2, pop_size)
     #Grab subset of population to make game run faster
-    NUM_PLAYERS = 35
+    NUM_PLAYERS = 100
     for i in range(0, pop_size-1, NUM_PLAYERS):
         sub_players = players[i:i+NUM_PLAYERS]
         play_game(sub_players)
@@ -247,4 +247,4 @@ def ga(pop_size, cross_rate=0.7, mut_rate=0.03, max_iters=20, net_units=8, net_u
 
 
     
-ga(105, mut_rate=0.3, cross_rate=0.3, max_iters=2)
+ga(200, mut_rate=0.3, cross_rate=0.5, max_iters=200)
