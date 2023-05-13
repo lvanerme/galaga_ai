a104f70a7854062fe1c156ccf1b4d9f256ffcebc
lvanerme <103009757+lvanerme@users.noreply.github.com>
2023-05-13 11:26:09 -0400

    Comiting to pull

--- ga.py
+++ ga.py
@@ -165,7 +165,7 @@ def ga(pop_size, cross_rate=0.7, mut_rate=0.03, max_iters=20, net_units=8, net_u
     start = time.time()
     players = gen_seed(net_units, net_units2, pop_size)
     #Grab subset of population to make game run faster
-    NUM_PLAYERS = 75
+    NUM_PLAYERS = 50
     for i in range(0, pop_size-1, NUM_PLAYERS):
         sub_players = players[i:i+NUM_PLAYERS]
         play_game(sub_players)
@@ -240,4 +240,4 @@ def ga(pop_size, cross_rate=0.7, mut_rate=0.03, max_iters=20, net_units=8, net_u
 
 
     
-ga(150, mut_rate=0.3, cross_rate=0.3, max_iters=2)
+ga(100, mut_rate=0.3, cross_rate=0.7, max_iters=100)
