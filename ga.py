5c26068e09d4a787c4320b9d34fe3c82686184f4
lvanerme <103009757+lvanerme@users.noreply.github.com>
2023-05-13 22:22:04 -0400

    commiting to merge

--- ga.py
+++ ga.py
@@ -137,7 +137,7 @@ def ga(pop_size, cross_rate=0.7, mut_rate=0.03, max_iters=20, net_units=8, net_u
     start = time.time()
     players = gen_seed(net_units, net_units2, pop_size)
     #Grab subset of population to make game run faster
-    NUM_PLAYERS = 10
+    NUM_PLAYERS = 35
     for i in range(0, pop_size-1, NUM_PLAYERS):
         sub_players = players[i:i+NUM_PLAYERS]
         play_game(sub_players)
